74c911389070edc1b8d67214536ea5830b0ab371
mohammedbellogithub <52636929+mohammedbellogithub@users.noreply.github.com>
2021-06-19 09:52:30 +0100

    Add files via upload

---
+++ Assignment/EnterName.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public class EnterName
+    {
+
+        public event EventHandler<BannedUserEventArgs> ev_BannedUser;
+        public void Name()
+        {
+            Console.Write("Enter You Name : ");
+            string name = Console.ReadLine();
+
+            if ((name == "Jack" || name == "Steven" || name == "Mathew") && (ev_BannedUser != null))
+            {
+                ev_BannedUser(this, new BannedUserEventArgs(name));
+            }
+            else
+            {
+                Console.WriteLine("Welcome to " + name);
+            }
+        }
+
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            EnterName ename = new EnterName();
+            ename.ev_BannedUser += Alarm;
+            ename.Name();
+            Console.Read();
+        }
+
+        static void Alarm(object sender, BannedUserEventArgs e)
+        {
+            Console.WriteLine("{0} Users Found. Sending Email to Administration.", e.Name);
+            Console.WriteLine("Email Sent.");
+            Console.WriteLine("Warning Alarm Started.");
+        }
+    }
+}
